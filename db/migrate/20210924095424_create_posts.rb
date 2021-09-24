a531ddca49521fd828c768a7f00402a7bfc56e06
tarushi-k <arrow0905@gmail.com>
2021-09-24 19:47:06 +0900

    postsテーブルのカラム作成

--- db/migrate/20210924095424_create_posts.rb
+++ db/migrate/20210924095424_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.text :memo  #t.に続くのがカラムの型、シンボルで記載されるのがカラム名
+      t.text :content  #t.に続くのがカラムの型、シンボルで記載されるのがカラム名
       t.timestamps
     end
   end
