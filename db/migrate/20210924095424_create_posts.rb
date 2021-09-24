d8052bd5d81191ecd392d870370d18510992f6ab
tarushi-k <arrow0905@gmail.com>
2021-09-24 19:04:44 +0900

    postモデルとpostsテーブルの作成

---
+++ db/migrate/20210924095424_create_posts.rb
@@ -0,0 +1,8 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.text :memo  #t.に続くのがカラムの型、シンボルで記載されるのがカラム名
+      t.timestamps
+    end
+  end
+end
